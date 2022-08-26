5348a5d0bde19826bb4d3c914f582ccf08a08841
whilini <whi0306@naver.com>
2022-08-26 13:49:15 +0900

    feat: create Nav top menu, add fonts, dark theme

--- src/components/NavBottom.jsx
+++ src/components/NavBottom.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const NavBottom = () => {
-  return <div>NavBottom</div>
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        backgroundColor: '#55A3FF',
+        fontSize: '20px',
+        height: '65px',
+        width: '100vw',
+        textAlign: 'center',
+      }}
+    >
+      NavBottom
+    </div>
+  )
 }
 
 export default NavBottom
