e3ba4c65d0a0b5188799c10ed24a8ace14109e88
yueunhye <yulll035@naver.com>
2022-08-28 00:54:08 +0900

    remove: Remove ...

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import Search from '~/components/Search'
 
 const Products = () => {
   return (
   <div>
-    {/* Products */}
-    <Outlet />
+    < Search />
   </div>
   )
 }
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -9,7 +9,7 @@ import UserInfo from '~/components/UserInfo'
 import Consume from '~/pages/Consume'
 import Products from '~/pages/Products'
 import Recomendation from '~/pages/Recomendation'
-import Search from '~/components/Search'
+
 
 
 const AppRouter = () => {
@@ -20,10 +20,7 @@ const AppRouter = () => {
         <Route path='/' element={<Main />} />
         <Route path='/recomendation' element={<Recomendation />} />
         <Route path='/auth' element={<Auth />} />
-        <Route path='/products' element={<Products />} />
-        <Route path="/products" element={<Products />} >
-          <Route index element={<Search />} />
-        </Route>
+        <Route path="/products" element={<Products />} />
         <Route path='/userinfo' element={<UserInfo />} />
         <Route paht='/wisdom' element={<Consume />} />
       </Routes>
