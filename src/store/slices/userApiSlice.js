86eda29a86ba23543b5a7a49fec694198b1863d2
whilini <whi0306@naver.com>
2022-09-06 13:34:30 +0900

    fix: logout error & change navTop router path

--- src/store/slices/userApiSlice.js
+++ src/store/slices/userApiSlice.js
@@ -1,3 +1,4 @@
+import { getCookie } from '../../utils/cookie'
 import { apiSlice } from '../api/apiSlice'
 
 export const userApiSlice = apiSlice.injectEndpoints({
@@ -6,65 +7,69 @@ export const userApiSlice = apiSlice.injectEndpoints({
       query: ({ email, password }) => ({
         url: 'login',
         method: 'POST',
-        body: { email, password },
+        body: { email, password }
       }),
       transformResponse: response => {
         return response.data
-      },
+      }
     }),
     signUp: builder.mutation({
       query: data => ({
         url: 'join',
         method: 'POST',
-        body: { ...data },
-      }),
+        body: { ...data }
+      })
     }),
     refreshData: builder.mutation({
       query: data => ({
         url: 'reissue',
         method: 'POST',
-        body: { ...data },
+        body: { ...data }
       }),
       transformResponse: response => {
         return response.data
-      },
+      }
     }),
     userDetail: builder.mutation({
       query: data => ({
         url: 'join/detail',
         method: 'POST',
-        body: { ...data },
-      }),
+        body: { ...data }
+      })
     }),
     detailPass: builder.query({
       query: () => ({
         url: 'join/detail-skip',
-        method: 'GET',
-      }),
+        method: 'GET'
+      })
     }),
     inquireUserData: builder.query({
       query: () => ({
         url: 'members',
-        method: 'GET',
+        method: 'GET'
       }),
       transformResponse: response => {
         return response.data
-      },
+      }
     }),
     editUserData: builder.mutation({
       query: data => ({
         url: 'members',
         method: 'PATCH',
-        body: { ...data },
-      }),
+        body: { ...data }
+      })
     }),
     logout: builder.mutation({
       query: () => ({
         url: 'logout',
         method: 'POST',
-      }),
-    }),
-  }),
+        body: {
+          accessToken: getCookie('accessToken'),
+          refreshToken: getCookie('refreshToken')
+        }
+      })
+    })
+  })
 })
 
 export const {
@@ -75,5 +80,5 @@ export const {
   useDetailPassQuery,
   useInquireUserDataQuery,
   useEditUserDataMutation,
-  useLogoutMutation,
+  useLogoutMutation
 } = userApiSlice
