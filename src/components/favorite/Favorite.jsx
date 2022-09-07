763a48a20e3a05b9a27697dfbc33edfa67e0eb9b
chan <cham9994@gmail.com>
2022-09-08 01:44:07 +0900

    feat: favorite count card

--- src/components/favorite/Favorite.jsx
+++ src/components/favorite/Favorite.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useGetFavoriteQuery } from '../../store/api/favoriteApiSlice'
 import Card from '../Card'
 import FavoriteDetail from './FavoriteDetail'
+import style from '../../scss/Favorite.module.scss'
 const Favorite = () => {
   const { data: favorite, isLoading, isError } = useGetFavoriteQuery()
   console.log(favorite)
@@ -11,14 +12,14 @@ const Favorite = () => {
       {!favorite?.length == 0 ? (
         <>
           <FavoriteDetail />
-          <div style={{ marginTop: '80px', marginBottom: '70px' }}>
+          <div className={style.favorite}>
             {favorite?.map((item, index) => (
               <Card key={index} productData={item} />
             ))}
           </div>
         </>
       ) : (
-        <h1 style={{ marginTop: '300px' }}>즐겨찾기가 없습니다.</h1>
+        <h1 className={style.noData}>즐겨찾기가 없습니다.</h1>
       )}
     </div>
   )
