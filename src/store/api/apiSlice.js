d5e1ddaac57ba0c8cfcf64bf4f85e2e3158cad27
chan <cham9994@gmail.com>
2022-09-11 05:00:07 +0900

    deploy: remove env

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getCookie } from '../../utils/cookie'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL,
+  // baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: 'https://conan.pll0123.com/',
   credentials: 'include',
   prepareHeaders: headers => {
     const accessToken = getCookie('accessToken')
